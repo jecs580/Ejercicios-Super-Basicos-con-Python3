5339d8c4ec2599f26ed84ff7b047136a71f5776f
Jorge Callisaya <sanchezenrique580@gmail.com>
2019-08-07 00:10:27 -0400

    Creacion del archivo ex1.py, tambien colocando las primeras lineas que pide el enunciado

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("Hola mundo")
+print("Hola de nuevo")
+print("Me gusta escribir esto")
+print("Esto es divertido")
+print('Yay! Impresion')
+print("Prefereriria que 'no'.")
+print('"Dije" que no toques eso.')
