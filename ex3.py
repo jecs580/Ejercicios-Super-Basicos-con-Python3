bb578cd26f8e06bf2cdd45a78465f1df61d2449d
Jorge Callisaya <sanchezenrique580@gmail.com>
2019-08-08 14:57:36 -0400

    Creacion del archivo ex3.py, resolviendo el primer ejercicio de los ejercicios de estudio

---
+++ ex3.py
@@ -0,0 +1,22 @@
+print("Ahora contare mis pollos: ")
+# Calculo para contar las gallinas, vista con parentesis:25+(30/6)
+print("Gallinas: ", 25+30/6)
+# Calculo para contar los gallos, vista con parentesis: 100-((25*3)%4)
+print("Gallos: ",100-25*3%4)
+print("Ahora contare los huevos: ")
+# Calculo para contar los huevos, vista con parentesis: 3+2+1-5+(4%2)-(1/4)+6
+print(3+2+1-5+4%2-1/4+6)
+print("Es verdad que 3+2 < 5-7 ?")
+# Comparando que el lado derecho de la inecuacion sea mayor, si es cierto nos devolvera True o sino False 
+print(3+2<5-7)
+# Ejecuta las operaciones del ejercicio anterior por separado
+print("Que es 3+2 ? :", 3+2)
+print("que es 5-7 ? ",5-7)
+print("Oh, por eso es falso")
+print("que tal, un poco mas")
+# Comprobando si 5 es mayor a -2
+print ("Es mayor 5>-2: ", 5>-2)
+# Comprobando si 5 es mayor o igual a -2
+print ("Es mayor o igual  5>=-2: ", 5>=-2)
+# Comprobando si 5 es menor o igual -2
+print("Es menor o igual 5<=-2: ", 5<=-2)
